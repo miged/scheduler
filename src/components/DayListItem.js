2e2c2ac217263caf2137244920f51f1c77ba8f38
Miguel <mige.d@outlook.com>
2021-12-06 14:38:42 -0500

    implement button, daylist, and daylistitem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import './DayListItem.scss';
+
+export default function DayListItem(props) {
+  const buttonClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots == 0,
+  });
+
+  let spotsText = props.spots;
+  if (props.spots > 1) {
+    spotsText += ' spots remaining';
+  } else if (props.spots === 1) {
+    spotsText += ' spot remaining';
+  } else {
+    spotsText = 'no spots remaining';
+  }
+
+  return (
+    <li className={buttonClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{spotsText}</h3>
+    </li>
+  );
+}
