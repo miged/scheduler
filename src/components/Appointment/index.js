a83c852ef8c7ff9a824740e9f8ced19d7f5c1ed5
Miguel <mige.d@outlook.com>
2021-12-06 18:24:37 -0500

    add appointment components

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.scss';
+
+import Header from './Header';
+import Empty from './Empty';
+import Show from './Show';
+import Confirm from './Confirm';
+import Status from './Status';
+import Error from './Error';
+
+export default function Appointment(props) {
+  const text = props.time ? `Appointment at ${props.time}` : 'No appointments'
+  return (
+    <article className="appointment">
+      {text}
+    </article>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,14 @@ import DayList from 'components/DayList';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
 
+import Appointment from 'components/Appointment/index';
+import Header from 'components/Appointment/Header';
+import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
+import Error from 'components/Appointment/Error';
+
 storiesOf('Button', module)
   .addParameters({
     backgrounds: [{ name: 'dark', value: '#222f3e', default: true }],
@@ -125,3 +133,33 @@ storiesOf('InterviewerList', module)
       onChange={action('setInterviewer')}
     />
   ));
+
+storiesOf('Appointment', module)
+  .addParameters({
+    backgrounds: [{ name: 'white', value: '#fff', default: true }],
+  })
+  .add('Appointment', () => <Appointment />)
+  .add('Appointment with Time', () => <Appointment time="12pm" />)
+  .add('Header', () => <Header time="12pm" />)
+  .add('Empty', () => <Empty onAdd={action('onAdd')} />)
+  .add('Show', () => (
+    <Show
+      student="John Doe"
+      interviewer={interviewers[0].name}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
+  ))
+  .add('Confirm', () => (
+    <Confirm
+      message="Delete the appointment?"
+      onConfirm={action('onConfirm')}
+      onCancel={action('onCancel')}
+    />
+  ))
+  .add('Status', () => (
+    <Status message="Deleting"/>
+  ))
+  .add('Error', () => (
+    <Error message="Could not delete appointment" onClose={action('onClose')}/>
+  ))
