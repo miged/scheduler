29833e9f72f36237460f92676262056d86be32eb
Miguel <mige.d@outlook.com>
2021-12-08 12:48:27 -0500

    add transition helper functions

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (replace) {
+      history.pop();
+    }
+    history.push(newMode);
+    setMode(newMode);
+  }
+  function back() {
+    if (history.length !== 1) {
+      setMode(history[history.length - 2]);
+      history.pop();
+    }
+  }
+
+  return { mode, transition, back };
+}
