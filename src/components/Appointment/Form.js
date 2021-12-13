7662295c23a77512fc5c687fc02710c50804c9d9
Miguel <mige.d@outlook.com>
2021-12-13 03:04:07 -0500

    add tests and form validation

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,16 +5,27 @@ import InterviewerList from 'components/InterviewerList';
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState('');
 
   const reset = () => {
     setStudent('');
     setInterviewer(null);
+    setError('');
   };
 
   const cancel = () => {
     reset();
     props.onCancel();
   };
+
+  function validate() {
+    if (student === '') {
+      setError('Student name cannot be blank');
+      return;
+    }
+    setError('');
+    props.onSave(student, interviewer);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -27,7 +38,9 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             onChange={(event) => setStudent(event.target.value)}
             value={student}
+            data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={props.interviewers}
@@ -40,7 +53,7 @@ export default function Form(props) {
           <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>
+          <Button confirm onClick={() => validate()}>
             Save
           </Button>
         </section>
